5e11fbf4173ff4069454f81f0869828bcd7d1974
Nejc Zupec <zupec.nejc@gmail.com>
2025-04-15 23:19:47 +0200

    Adjust static config

--- demo_project/settings.py
+++ demo_project/settings.py
@@ -120,11 +120,6 @@ STATIC_URL = 'static/'
 # Directory where collectstatic will collect static files for deployment
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Additional locations of static files
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
